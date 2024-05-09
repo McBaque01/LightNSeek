f052ea24f9ba28c5026d7419b2f1b86b2bcb7be1
McBaque <mcbaque01@gmail.com>
2024-05-10 01:03:11 +0800

    Trying changes online!

--- Frontend/LightNSeek/src/App.tsx
+++ Frontend/LightNSeek/src/App.tsx
@@ -1,6 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+
+
 
 function App() {
+
+  interface DisplayerPropsTypes {
+  
+    sequence: number[];
+  }
 
   function getRandomInt(min: number, max: number): number {
     min = Math.ceil(min);
@@ -8,66 +16,34 @@ function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  // const randomNumber = getRandomInt(0, 8);
-
-  // console.log(randomNumber)
 
   const ParentRef = useRef<HTMLDivElement | null>(null);
   const [pick, setPick] = useState<boolean>(false);
-  const [sequence, setSequence] = useState<number[]>([])
-  const [playerSequence, setPlayerSequence] =  useState<number[]>([]);
-
-  const handleClick = () => {
-    let randomNumber = getRandomInt(0, 8);
-
-    setSequence([...sequence, randomNumber])
-      // sequence.push(randomNumber)
-      console.log(sequence)
-  }
+ 
+  const [onDisplay, setOnDisplay] = useState<boolean>(false);
+  const [time, setTime] = useState(4)
   
+  let playerSequence: number[] = [];
+  let sequence: number[] = []
+  const round: number = 1
+  const handleClick = () => {
+    let randomNumber = getRandomInt(0, 8)
+    
 
-  useEffect(()=>{
+    sequence.push(randomNumber);
+      console.log(sequence, "Sequence")
+      handleDisplayer({sequence});
+  }
 
+ 
+
+  const handleDisplayer =  ({sequence}:DisplayerPropsTypes) => {
 
     const parentElement = ParentRef.current;
     const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
-    console.log(sequence, "first render")
-
-      // for(let i = 0; i <  (childrens?.length ?? 0); i++){
-
-      //   for(let j = 0;  j < sequence.length; j++){
-      //     let current = childrens ? childrens[i] : undefined;
-      //     if (current) {
-      //       current.style.backgroundColor = "red";
-      //       setTimeout(() => {
-      //         current.style.backgroundColor = ""; // Revert back to the original state
-      //       }, 1000);
-      //     }
-      //   }
-
-      // }
-
-      // for(let i = 0; i < sequence.length; i++){
-
-      //   console.log(sequence[i],"S")
-      //     let current = childrens ? childrens[sequence[i]] : undefined;
-      //     console.log(current, "Current")
-      //     if (current) {
-      //       current.style.backgroundColor = "red";
-      //       setTimeout(() => {
-      //         current.style.backgroundColor = ""; // Revert back to the original state
-      //         console.log(current, "AFTER")
-      //       }, 1000);
-      //     }
-        
-
-      // }
-
-      // let randomNumber = getRandomInt(0, 8);
-      // sequence.push(randomNumber)
-      // console.log(sequence, "first")
-
-
+    
+    setTimeout(() => {
+    
       for (let i = 0; i < sequence.length; i++) {
         const index = sequence[i];
         setTimeout(() => {
@@ -80,76 +56,152 @@ function App() {
                     console.log(current, "AFTER");
                 }, 500);
             }
-        }, i * 700); // Delay each iteration by 1000 milliseconds
+        }, i * 600); // Delay each iteration by 1000 milliseconds
       }
-
-
-
-    //   for(let i = 0; i <  sequence.length; i++){
-    //     for(let j = 0; j < (childrens?.length ?? 0); j++){
-    //       let current = childrens ? childrens[i] : undefined;
-    //       console.log(current, "Current")
-    //       if (current) {
-    //         current.style.backgroundColor = "red";
-    //         setTimeout(() => {
-    //           current.style.backgroundColor = ""; // Revert back to the original state
-    //           console.log(current, "AFTER")
-    //         }, 1000);
-    //       }
-    //   }
-
-    // }
-
-
-
-      // for(let i = 0; i < sequence.length; i++){
-      //   console.log(sequence[i])
-      // }
-
-
-
-      // const firstChild = childrens ? childrens[1] : undefined;
-      // if (firstChild) {
-      //   firstChild.style.backgroundColor = "red";
-      //   setTimeout(() => {
-      //     firstChild.style.backgroundColor = ""; // Revert back to the original state
-      //     // console.log(firstChild.style.backgroundColor, "after")
-      // }, 1000);
-      // }
-
-      // console.log(childrens);
+     
       
-
-  },[sequence])
+    }, 1000);
+    
+   
+  }
   
 
-  // useEffect(()=>{
-  //   let randomNumber = getRandomInt(0, 8);
-
-  //   sequence.push(randomNumber)
-  //   // console.log(randomNumber)
-  //   // console.log(sequence)
-  //   console.log(sequence)
-
-  // },[pick])
-
-  console.log(playerSequence)
+  useEffect(()=>{
+      setTimeout(() => {
+       handleClick();
+      }, 1000);
+    
+  },[]);
+    
   
   return (
-    <div className="bg-slate-900 w-full h-screen justify-center items-center flex ">
+    <div className="bg-slate-900 w-full h-screen justify-center items-center flex flex-col">
+      <h1 className=' text-red-100 text-[4em]'>ROUND: {round}</h1>
+      <h1 className=' text-red-100 text-[4em]'>time:{time}</h1>
       {/* Container for the three divs */}
       <div className="bg-slate-900 w-fit h-fit gap-2 justify-center items-center grid grid-cols-3" ref={ParentRef}>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 0])} >0</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 1])}>1</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 2])}>2</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 3])}>3</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 4])}>4</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 5])}>5</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 6])}>6</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 7])}>7</div>
-        <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>setPlayerSequence([...playerSequence, 8])}>8</div>
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(0);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            0
+          </button>
+        </div>
+        
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(1);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            1
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(2);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            2
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(3);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            3
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(4);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            4
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(5);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            5
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(6);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            6
+          </button>
+        </div>
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(7);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            7
+          </button>
+        </div>
+
+
+        <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
+          <button 
+            className='h-full w-full'  
+            onClick={() => {
+              if (onDisplay) {
+                playerSequence.push(8);
+                console.log(playerSequence);
+              }
+            }}
+          >
+            8
+          </button>
+        </div>
+
+        {/* <div style={{  }} className="bg-white w-[8em] h-[8em]" onClick={()=>{playerSequence.push(8),console.log(playerSequence)}}>8</div> */}
       </div>
-      <button onClick={()=>handleClick()}>REPICK!</button>
+      <button className=' text-red-100 text-[1.4em]' onClick={()=>handleClick()}>Start</button>
     </div>
   );
 }
