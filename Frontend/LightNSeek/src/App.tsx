55305e544a89ea37785c32cf9304ca29269b7c5c
McBaque <mcbaque01@gmail.com>
2024-05-11 00:25:15 +0800

    Complete Game process!

--- Frontend/LightNSeek/src/App.tsx
+++ Frontend/LightNSeek/src/App.tsx
@@ -1,10 +1,8 @@
 import { useState, useRef } from 'react';
 
-
 function App() {
 
   interface DisplayerPropsTypes {
-  
     sequence: number[];
   }
 
@@ -14,117 +12,180 @@ function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-
   const ParentRef = useRef<HTMLDivElement | null>(null);
-  // const [pick, setPick] = useState<boolean>(false);
- 
-  const [onDisplay, setOnDisplay] = useState<boolean>(false);
-  
-  const [Typing, setTyping] = useState<boolean>(false);
-  // const [sequence, setSequence] = useState<number[]>([])
+
+
   const [round, setRound] = useState<number>(1)
   const [sequence, setSequence] = useState<number[]>([]);
   const [playerSequence, setPlayerSequence] = useState<number[]>([]);
 
   const [countdown, setCountdown] = useState(3);
-  const [isPlaying, setPlaying] =useState<boolean> (false)
+  const [isPlaying, setPlaying] = useState<boolean> (false)
+  const [isWin, setIsWin] = useState<boolean> (false)
+
 
   
 
-  const handleMatch = () => {
-    const isMatch = sequence.length === playerSequence.length && sequence.every((value, index) => value === playerSequence[index])
-    console.log(isMatch)
-    
-    if (isMatch) {
-      setPlayerSequence([]);
-      setRound(prev=>prev+1)
-    
-      handleNextRound();
-    } else {
-      setPlayerSequence([])
-      setSequence([]);
-      setRound(1);
-      setCountdown(3);
-      setPlaying(false)
-    }
+  const handleWinDisplay = () =>{
+    const parentElement = ParentRef.current;
+    const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
 
+    if (childrens) {
+      setTimeout(()=>{
+       
+        Array.from(childrens).forEach(child => {
+            child.style.backgroundColor = "green";
+        });
+
+      },200)
+      
+     }
+    
+     setTimeout(()=>{
+       if (childrens) {
+        
+         Array.from(childrens).forEach(child => {
+             child.style.backgroundColor = ""; 
+         });
+       }
+ 
+     },800)
+     
+   }
+
+
+   const handleLoseDisplayer = () => {
+    const parentElement = ParentRef.current;
+    const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
+
+    if (childrens) {
+      setTimeout(()=>{     
+        Array.from(childrens).forEach(child => {
+            child.style.backgroundColor = "red"; 
+        });
+      },200)
+      
+     }
+    
+     setTimeout(()=>{
+       if (childrens) {    
+         Array.from(childrens).forEach(child => {
+             child.style.backgroundColor = ""; 
+         });
+       }
+     },800)
+    
+   }
+
+
+   const handleDisplayer =  ({sequence}:DisplayerPropsTypes) => {
+
+    const parentElement = ParentRef.current;
+    const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
+
+      let completedTimeouts = 0;
+      
+      setPlaying(false)
+     
+      setTimeout(() => {
+      
+        for (let i = 0; i < sequence.length; i++) {
+          const index = sequence[i];
+          setTimeout(() => {
+              const current = childrens ? childrens[index] : undefined;
+             
+              if (current) {
+                  current.style.backgroundColor = "red";
+                  setTimeout(() => {
+                      current.style.backgroundColor = ""; // Revert back to the original state
+                    
+                      completedTimeouts++; // Increment the counter
+                      if (completedTimeouts === sequence.length) {
+                          setPlaying(true); // Call setPlaying(true) when all timeouts are completed
+                      }
+                  }, 500);
+              }
+          }, i * 600); // Delay each iteration by 1000 milliseconds
+        }
+       
+        
+      }, 1000);
+  
+    
+    }
+  
+
+  const handleMatch = (pSequence: number[]) => {
+    const isMatch = sequence.length === pSequence.length && sequence.every((value, index) => value === pSequence[index])
+   
+    console.log(isMatch)
     return isMatch;
   }
 
-  const handleMouseEnter = () => {
-    if (!isPlaying) return; // Disable the event handler if not playing
-    setTyping(true);
-    // Any other logic when mouse enters
-  };
 
-  const handleMouseLeave = () => {
-    if (!isPlaying) return; // Disable the event handler if not playing
-    setTyping(false);
-    setOnDisplay(false);
-    handleMatch();
-    // Any other logic when mouse leaves
-  };
+  const handleClickBlink = (inputNumber: number) => {
+    const parentElement = ParentRef.current;
+    const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
 
-  // const handleIsWin = (sequence: number[], playerSequence:number[]) => {
-  //     console.log(sequence, "SEQUENCE")
-  //     console.log(playerSequence, "PLAYERSEQUENCE")
-  //   const result = sequence == playerSequence ? true: false;
-  //   console.log(result)
-  //   return result;
-  // }
-   
-//   let playerSequence: number[] = [];
-//  let sequence: number[] = []
-  // const round: number = 1
+    const BlinkMe = childrens ? childrens[inputNumber] : null;
 
+    if (BlinkMe) {
+      BlinkMe.style.backgroundColor = "red";
+      setTimeout(() => {
+        BlinkMe.style.backgroundColor = ""; // Revert back to the original state
+         
+      }, 200);
+  }
 
-  // const handleClick = () => {
-  //   let randomNumber = getRandomInt(0, 8)
-  //   setSequence((prev)=>[...prev, randomNumber] )
-  //   // sequence.push(randomNumber);
-  //     console.log(sequence, "Sequence")
+  }
 
 
 
-  //     // const countdownTimeout = setTimeout(() => {
-  //     //     const countdownInterval = setInterval(() => {
-  //     //       if (time > 0) {
-  //     //         setTime(prevTime => prevTime - 1);
-  //     //         console.log(countdownTimeout)
-  //     //       }
-  //     //     }, 1000);
+  const handleClick = (inputNumber: number) => {
+
     
-  //     //     setTimeout(() => {
-  //     //       clearInterval(countdownInterval);
-  //     //     handleDisplayer({sequence})
-  //     //     }, time * 1000); // Executes handleClick after the countdown finishes
-  //     //   }, 0); // Start the countdown after 2 seconds
 
-      
-  //     // handleDisplayer({sequence});
-  // }
+    handleClickBlink(inputNumber)
 
-  // const handleClick = () => {
-  //   let randomNumber = getRandomInt(0, 8);
-  //   setSequence(prev => [...prev, randomNumber]);
-  // };
 
-  // const handleStart = () => {
-  //   // Countdown logic
-  //   const countdownInterval = setInterval(() => {
-  //     setCountdown(prevCountdown => prevCountdown - 1);
-  //   }, 1000);
+    console.log(inputNumber , "YOUR INPUT")
 
-  //   // After 3 seconds, call handleDisplayer
-  //   setTimeout(() => {
-  //     clearInterval(countdownInterval); // Stop the countdown
-  //     handleDisplayer({ sequence });
-  //   }, 3000);
-  // };
+    const updatedSequence = [...playerSequence, inputNumber];
+    setPlayerSequence(updatedSequence);
+    console.log(updatedSequence,"PLAYER")
+    console.log(sequence,"SEQUENCE")
+
+
+    if(updatedSequence.length == sequence.length){
+      if(handleMatch(updatedSequence)){
+        handleWinDisplay();
+        setPlayerSequence([])
+        setRound(prev=>prev+1)
+        handleNextRound();
+        // handleWinDisplay();
+        return "WIN"
+      }else{
+        handleLoseDisplayer();
+        setPlayerSequence([])
+        setSequence([]);
+        setRound(1);
+        setCountdown(3);
+        setPlaying(false)
+        setIsWin(false)
+        return "LOSE"
+      }
+    }
+
+    
+
+  }
+
+
 
   const handleStart = () => {
+    setIsWin(true);
 
-    setPlaying(true);
+    setPlaying(false)
     // Countdown logic
     const countdownInterval = setInterval(() => {
       setCountdown(prevCountdown => prevCountdown - 1);
@@ -140,6 +201,8 @@ function App() {
 
     }, 3000);
 
+    setCountdown(3)
+
   };
 
   const handleNextRound = () => {
@@ -149,98 +212,31 @@ function App() {
     setSequence(updatedSequence);
     handleDisplayer({ sequence: updatedSequence });
   }
-  
 
  
-
-  const handleDisplayer =  ({sequence}:DisplayerPropsTypes) => {
-
-    const parentElement = ParentRef.current;
-    const childrens = parentElement ? (parentElement.children as HTMLCollectionOf<HTMLDivElement> | undefined) : undefined;
-    
-    setTimeout(() => {
-    
-      for (let i = 0; i < sequence.length; i++) {
-        const index = sequence[i];
-        setTimeout(() => {
-            const current = childrens ? childrens[index] : undefined;
-            console.log(current, "Current");
-            if (current) {
-                current.style.backgroundColor = "red";
-                setTimeout(() => {
-                    current.style.backgroundColor = ""; // Revert back to the original state
-                    console.log(current, "AFTER");
-                }, 500);
-            }
-        }, i * 600); // Delay each iteration by 1000 milliseconds
-      }
-     
-      
-    }, 1000);
-
-    setOnDisplay(true)
-    
-  }
-
-
-  // useEffect(() => {
-
-  //   // handleDisplayer({sequence})
-  // }, [sequence]);
-
-  // useEffect(() => {
-  //   console.log(sequence, "Sequence");
-  //   let randomNumber = getRandomInt(0, 8);
-  //   setSequence(prev => [...prev, randomNumber]);
-  // }, [round]);
-
+  // console.log(sequence, "Sequencee")
+  // console.log(playerSequence, "PlayerSeq")
+  // console.log(isPlaying, "PLAYING?")
   
-  // useEffect(() => {
-  //   const countdownTimeout = setTimeout(() => {
-  //     const countdownInterval = setInterval(() => {
-  //       if (time > 0) {
-  //         setTime(prevTime => prevTime - 1);
-  //         console.log(countdownTimeout)
-  //       }
-  //     }, 1000);
-
-  //     setTimeout(() => {
-  //       clearInterval(countdownInterval);
-  //       handleClick();
-  //     }, time * 1000); // Executes handleClick after the countdown finishes
-  //   }, 0); // Start the countdown after 2 seconds
-     
-  // }, []);
-  
-  console.log(Typing, "isTyping")
-  console.log(onDisplay, "isDISPLAY")
-  console.log(sequence, "Sequencee")
-  console.log(playerSequence, "PlayerSeq")
-  
-  
+  // console.log(handleWinDisplay());
   
   return (
     <div className="bg-slate-900 w-full h-screen justify-center items-center flex flex-col">
        <h1 className=' text-red-100 text-[4em]'>Round:{round}</h1>
       <h1 className=' text-red-100 text-[4em]'>{countdown === 0 ? 'START' : countdown}</h1>
      
-      {/* Container for the three divs */}
-      <div className='bg-green-400 p-1' 
-      onMouseEnter={handleMouseEnter} 
-      onMouseLeave={handleMouseLeave}
-    
       
+      <div className='bg-green-400 p-1' 
+      
+
       >
         <div className="bg-slate-900 w-fit h-fit gap-2 justify-center items-center grid grid-cols-3" ref={ParentRef}>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 0])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full'
+             disabled={!isPlaying} 
+              onClick={() => {handleClick(0)}}
+             
             >
               0
             </button>
@@ -248,38 +244,26 @@ function App() {
           
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
             <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 1])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+              disabled={!isPlaying} 
+               onClick={() => {handleClick(1)}}
             >
               1
             </button>
           </div>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 2])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full'
+             disabled={!isPlaying} 
+              onClick={() => {handleClick(2)}}
             >
               2
             </button>
           </div>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 3])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full'
+           disabled={!isPlaying} 
+               onClick={() => {handleClick(3)}}
             >
               3
             </button>
@@ -287,51 +271,36 @@ function App() {
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
             <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 4])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+              disabled={!isPlaying}  
+               onClick={() => {handleClick(4)}}
             >
               4
             </button>
           </div>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 5])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full'
+              disabled={!isPlaying} 
+               onClick={() => {handleClick(5)}}
             >
               5
             </button>
           </div>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 6])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full'
+              disabled={!isPlaying}  
+               onClick={() => {handleClick(6)}}
             >
               6
             </button>
           </div>
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
-            <button className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 7])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+            <button className='h-full w-full' 
+
+                disabled={!isPlaying}   
+               onClick={() => {handleClick(7)}}
             >
               7
             </button>
@@ -341,13 +310,9 @@ function App() {
 
           <div style={{}} className="bg-white w-[8em] h-[8em] relative flex justify-center p-2w">
             <button 
-              className='h-full w-full'  
-              onClick={() => {
-                if (onDisplay) {
-                  setPlayerSequence(prev=>[...prev, 8])
-                  console.log(playerSequence, "BUTTON CLICK");
-                }
-              }}
+              className='h-full w-full' 
+              disabled={!isPlaying}  
+              onClick={() => {handleClick(8)}}
             >
               8
             </button>
@@ -360,7 +325,7 @@ function App() {
 
       <button className=' text-red-100 text-[1.4em]' onClick={handleStart} 
       
-      disabled={isPlaying === true}
+      disabled={isWin}
       >Start</button>
     </div>
   );
