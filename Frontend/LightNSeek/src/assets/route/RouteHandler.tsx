174f01797ec79b9b56d3be9602f98b6d877e4326
McBaque <mcbaque01@gmail.com>
2024-05-11 02:17:33 +0800

    Commiting fixed  potentials errors

--- Frontend/LightNSeek/src/assets/route/RouteHandler.tsx
+++ Frontend/LightNSeek/src/assets/route/RouteHandler.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate,} from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { Landing } from '../component/Landing'
 import { Hero } from '../component/Hero'
 
@@ -7,7 +7,7 @@ export const RouteHandler = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
+
         <Route path="/" element={<Landing/>} />
         <Route path='/Hero' element={<Hero/>}/>
          
